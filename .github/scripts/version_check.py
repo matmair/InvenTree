matmair/InvenTree

commit 6f67fb2c9a952cdd9caba9ae159792581d157431

Make versions_check more reliable in forks (#7785)
* discover repo
only run in GH actions
use discovered api
this is usefull for GHES enviroments
style fixes

* Fix wrong env name

.github/scripts/version_check.py
@@ -18,6 +18,9 @@ from pathlib import Path
 
 import requests
 
+REPO = os.getenv('GITHUB_REPOSITORY', 'inventree/inventree')
+GITHUB_API_URL = os.getenv('GITHUB_API_URL', 'https://api.github.com')
+
 
 def get_existing_release_tags():
     """Request information on existing releases via the GitHub API."""
@@ -28,9 +31,7 @@ def get_existing_release_tags():
     if token:
         headers = {'Authorization': f'Bearer {token}'}
 
-    response = requests.get(
-        'https://api.github.com/repos/inventree/inventree/releases', headers=headers
-    )
+    response = requests.get(f'{GITHUB_API_URL}/repos/{REPO}/releases', headers=headers)
 
     if response.status_code != 200:
         raise ValueError(
@@ -90,6 +91,11 @@ def check_version_number(version_string, allow_duplicate=False):
 
 
 if __name__ == '__main__':
+    # Ensure that we are running in GH Actions
+    if os.environ.get('GITHUB_ACTIONS', '') != 'true':
+        print('This script is intended to be run within a GitHub Action!')
+        sys.exit(1)
+
     if 'only_version' in sys.argv:
         here = Path(__file__).parent.absolute()
         version_file = here.joinpath(
@@ -102,14 +108,13 @@ if __name__ == '__main__':
             results[0] = str(int(results[0]) - 1)
         print(results[0])
         exit(0)
+
     # GITHUB_REF_TYPE may be either 'branch' or 'tag'
     GITHUB_REF_TYPE = os.environ['GITHUB_REF_TYPE']
 
     # GITHUB_REF may be either 'refs/heads/<branch>' or 'refs/heads/<tag>'
     GITHUB_REF = os.environ['GITHUB_REF']
-
     GITHUB_REF_NAME = os.environ['GITHUB_REF_NAME']
-
     GITHUB_BASE_REF = os.environ['GITHUB_BASE_REF']
 
     # Print out version information, makes debugging actions *much* easier!
@@ -193,7 +198,7 @@ if __name__ == '__main__':
     # Ref: https://getridbug.com/python/how-to-set-environment-variables-in-github-actions-using-python/
     with open(os.getenv('GITHUB_ENV'), 'a') as env_file:
         # Construct tag string
-        tags = ','.join([f'inventree/inventree:{tag}' for tag in docker_tags])
+        tags = ','.join([f'{REPO}:{tag}' for tag in docker_tags])
 
         env_file.write(f'docker_tags={tags}\n')
 

commit abddd8bc3dfe275936397141d9758ec2d00fae8c

Oci labels (#211)
* adapt namespace

* add new labels

* make baseimage available for labels

* remove unneeded ending

* ensure image name is correct for ghcrio

* ensure the right outputs are used

* fix reference

* fix assigment

* only push docker reg image if authd

* swith back to env

this gets provided by the version ci script

* fix repo caps (#7800)

fixes error introduced in https://github.com/inventree/InvenTree/pull/7785

* make repo targets changeable

* make readable

* revert ghcr.io change

.github/scripts/version_check.py
@@ -10,6 +10,7 @@ tagged branch:
 
 """
 
+import itertools
 import json
 import os
 import re
@@ -195,10 +196,13 @@ if __name__ == '__main__':
     print(f"Version check passed for '{version}'!")
     print(f"Docker tags: '{docker_tags}'")
 
+    target_repos = [REPO.lower(), f'ghcr.io/{REPO.lower()}']
+
     # Ref: https://getridbug.com/python/how-to-set-environment-variables-in-github-actions-using-python/
     with open(os.getenv('GITHUB_ENV'), 'a') as env_file:
         # Construct tag string
-        tags = ','.join([f'{REPO}:{tag}' for tag in docker_tags])
+        tag_list = [[f'{r}:{t}' for t in docker_tags] for r in target_repos]
+        tags = ','.join(itertools.chain(*tag_list))
 
         env_file.write(f'docker_tags={tags}\n')
 

.github/workflows/docker.yaml
@@ -135,9 +135,9 @@ jobs:
         id: docker_login
         run: |
           if [ -z "${{ secrets.DOCKER_USERNAME }}" ]; then
-            echo "skip_dockerhub_login=true" >> $GITHUB_ENV
+            echo "skip_dockerhub_login=true" >> $GITHUB_OUTPUT
           else
-            echo "skip_dockerhub_login=false" >> $GITHUB_ENV
+            echo "skip_dockerhub_login=false" >> $GITHUB_OUTPUT
           fi
       - name: Login to Dockerhub
         if: github.event_name != 'pull_request' && steps.docker_login.outputs.skip_dockerhub_login != 'true'

contrib/container/Dockerfile
@@ -11,6 +11,7 @@
 
 ARG base_image=python:3.11-alpine3.18
 FROM ${base_image} AS inventree_base
+ARG base_image
 
 # Build arguments for this image
 ARG commit_tag=""
@@ -48,13 +49,18 @@ ENV INVENTREE_BACKGROUND_WORKERS="4"
 ENV INVENTREE_WEB_ADDR=0.0.0.0
 ENV INVENTREE_WEB_PORT=8000
 
-LABEL org.label-schema.schema-version="1.0" \
-      org.label-schema.build-date=${DATE} \
-      org.label-schema.vendor="inventree" \
-      org.label-schema.name="inventree/inventree" \
-      org.label-schema.url="https://hub.docker.com/r/inventree/inventree" \
-      org.label-schema.vcs-url="https://github.com/inventree/InvenTree.git" \
-      org.label-schema.vcs-ref=${commit_tag}
+LABEL org.opencontainers.image.created=${DATE} \
+      org.opencontainers.image.vendor="inventree" \
+      org.opencontainers.image.title="InvenTree backend server" \
+      org.opencontainers.image.description="InvenTree is the open-source inventory management system" \
+      org.opencontainers.image.url="https://inventree.org" \
+      org.opencontainers.image.documentation="https://docs.inventree.org" \
+      org.opencontainers.image.source="https://github.com/inventree/InvenTree" \
+      org.opencontainers.image.revision=${commit_hash} \
+      org.opencontainers.image.licenses="MIT" \
+      org.opencontainers.image.base.name="docker.io/library/${base_image}" \
+      org.opencontainers.image.version=${commit_tag}
+
 
 # Install required system level packages
 RUN apk add --no-cache \