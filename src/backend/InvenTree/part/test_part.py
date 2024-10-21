efc633f9e8069b76fce41413b153f4319ac2b37d
Matthias Mair <code@mjmair.com>
2024-10-21 23:08:14 +0200

    update tests

--- src/backend/InvenTree/part/test_part.py
+++ src/backend/InvenTree/part/test_part.py
@@ -2,7 +2,6 @@
 
 import os
 
-from django.conf import settings
 from django.core.cache import cache
 from django.core.exceptions import ValidationError
 from django.test import TestCase
@@ -192,8 +191,7 @@ class PartTest(TestCase):
     def test_attributes(self):
         """Test Part attributes."""
         self.assertEqual(self.r1.name, 'R_2K2_0805')
-        if settings.ENABLE_CLASSIC_FRONTEND:
-            self.assertEqual(self.r1.get_absolute_url(), '/part/3/')
+        self.assertEqual(self.r1.get_absolute_url(), '/platform/part/3')
 
     def test_category(self):
         """Test PartCategory path."""
