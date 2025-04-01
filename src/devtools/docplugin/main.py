a92063fc6785de908f15609d07cc0bf107db8bba
Matthias Mair <code@mjmair.com>
2025-04-01 20:52:07 +0200

    include (mk) doc files during release

--- src/devtools/docplugin/main.py
+++ src/devtools/docplugin/main.py
@@ -1,5 +1,6 @@
 """Plugin to keep solitified markdown around."""
 
+import os
 from pathlib import Path
 
 import mkdocs
@@ -12,20 +13,31 @@ class MyPluginnConfig(base.Config):
 
     enabled = c.Type(bool, default=True)
     verbose = c.Type(bool, default=False)
-
-
-BASE = Path('base_src')
+    target = c.Type(str, default='')
 
 
 class MyPlugin(mkdocs.plugins.BasePlugin[MyPluginnConfig]):
     """Plugin to keep solitified markdown around."""
+
+    def on_config(self, config):
+        """Ensure base dir exists."""
+        cf_t = self.config['target']
+        if cf_t:
+            self.base = Path(cf_t)
+        elif os.environ.get('SOLIDIFY_DIR'):
+            self.base = Path(os.environ['SOLIDIFY_DIR'])
+        else:
+            self.base = Path('base_src')
+
+        if not self.base.exists():
+            self.base.mkdir(parents=True)
 
     def on_page_markdown(self, markdown: str, **kwargs) -> str:
         """Safe page mk."""
         if self.config['verbose']:
             print('Markdown content:', markdown)
         # Log file away
-        trg = BASE.joinpath(kwargs['page'].file.src_path)
+        trg = self.base.joinpath(kwargs['page'].file.src_path)
         if not trg.parent.exists():
             trg.parent.mkdir(parents=True)
         trg.write_text(markdown)
