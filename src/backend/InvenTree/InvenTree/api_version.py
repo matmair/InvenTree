3f97f1cb373c6825c61111bd5e259b24d2eedc56
Matthias Mair <code@mjmair.com>
2025-10-01 23:50:49 +0200

    find issues in network egress

--- src/backend/InvenTree/InvenTree/api_version.py
+++ src/backend/InvenTree/InvenTree/api_version.py
@@ -13,6 +13,7 @@ v393 -> 2025-09-01 : https://github.com/inventree/InvenTree/pull/10437
 v392 -> 2025-09-22 : https://github.com/inventree/InvenTree/pull/10374
     - Refactors 'part_detail', 'supplier_detail', 'manufacturer_detail'and 'pretty' param in SupplierPartList API endpoint
 
+
 v391 -> 2025-09-06 : https://github.com/inventree/InvenTree/pull/10279
     - Refactors 'exclude_tree', 'cascade', and 'location' filters in StockList API endpoint
 
