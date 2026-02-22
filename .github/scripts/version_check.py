2eda30ef957d143f3ceb5caf8cd4915731c608af
Matthias Mair <code@mjmair.com>
2026-02-22 18:12:05 +0100

    use pkg_channel (#550)
    * Update release workflow to use pkg_channel
    
    * Add pkg_channel for version tagging logic
    
    Added pkg_channel variable to manage package channel based on version tag or branch.

--- .github/scripts/version_check.py
+++ .github/scripts/version_check.py
@@ -264,6 +264,7 @@ def main() -> bool:
 
     # Determine which docker tag we are going to use
     docker_tags: Optional[list[str]] = None
+    pkg_channel = None
 
     if GITHUB_REF_TYPE == 'tag':
         # GITHUB_REF should be of the form /refs/heads/<tag>
@@ -278,10 +279,15 @@ def main() -> bool:
 
         docker_tags = [version_tag, 'stable'] if highest_release else [version_tag]
 
+        # Add release-line tag
+        release_line = '.'.join(version_tag.split('.')[:2])+'.x'
+        pkg_channel = release_line
+
     elif GITHUB_REF_TYPE == 'branch':
         # Otherwise we know we are targeting the 'master' branch
         docker_tags = ['latest']
         highest_release = False
+        pkg_channel = 'latest'
 
     else:
         print('Unsupported branch / version combination:')
@@ -310,6 +316,8 @@ def main() -> bool:
 
         if GITHUB_REF_TYPE == 'tag' and highest_release:
             env_file.write('stable_release=true\n')
+
+        env_file.write(f'pkg_channel={pkg_channel}\n')
     return True
 
 
